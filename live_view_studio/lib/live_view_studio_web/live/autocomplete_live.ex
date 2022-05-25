c38d577fa67d84d14b40ae29e8d171bde692e53f
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2022-05-25 16:05:23 +0000

    finished plain 8 section, no notes exercices

--- live_view_studio/lib/live_view_studio_web/live/autocomplete_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/autocomplete_live.ex
@@ -1,13 +1,16 @@
-defmodule LiveViewStudioWeb.SearchLive do
+defmodule LiveViewStudioWeb.AutocompleteLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Stores
+  alias LiveViewStudio.Cities
 
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
         zip: "",
+        city: "",
         stores: [],
+        matches: [],
         loading: false
       )
 
@@ -21,12 +24,27 @@ defmodule LiveViewStudioWeb.SearchLive do
 
     <form phx-submit="zip-search">
     <input type="text" name="zip" value={@zip} placeholder="Zip Code"
-      autofocus autocomplete="off" readonly={@loading} />
+      autofocus autocomplete="off" readonly={@loading}/>
 
       <button type="submit">
         <img src="images/search.svg" >
       </button>
     </form>
+
+    <form phx-submit="city-search" phx-change="suggest-city">
+    <input type="text" name="city" value={@city} placeholder="City name"
+       autocomplete="off" readonly={@loading} list="matches" phx-debounce="1000"/>
+      <button type="submit">
+        <img src="images/search.svg" >
+      </button>
+    </form>
+
+    <datalist id="matches">
+      <%= for match <- @matches do %>
+        <option value={match}><%= match %></option>
+      <% end %>
+    </datalist>
+
 
     <%= if @loading do %>
       <div class="loader">
@@ -68,6 +86,24 @@ defmodule LiveViewStudioWeb.SearchLive do
     """
   end
 
+  def handle_event("suggest-city", %{"city" => prefix}, socket) do
+    socket = assign(socket, matches: Cities.suggest(prefix))
+    {:noreply, socket}
+  end
+
+  def handle_event("city-search", %{"city" => city_code}, socket) do
+    send(self(), {:run_city_search, city_code})
+
+    socket =
+      assign(socket,
+        city: city_code,
+        loading: true,
+        stores: []
+      )
+
+    {:noreply, socket}
+  end
+
   def handle_event("zip-search", %{"zip" => zip_code}, socket) do
     send(self(), {:run_zip_search, zip_code})
 
@@ -79,6 +115,26 @@ defmodule LiveViewStudioWeb.SearchLive do
       )
 
     {:noreply, socket}
+  end
+
+  def handle_info({:run_city_search, city_code}, socket) do
+    case Stores.search_by_city(city_code) do
+      [] ->
+        socket =
+          socket
+          |> put_flash(:info, "No stores matching \"#{city_code}\"")
+          |> assign(stores: [], loading: false)
+
+        {:noreply, socket}
+
+      stores ->
+        socket =
+          socket
+          |> clear_flash()
+          |> assign(stores: stores, loading: false)
+
+        {:noreply, socket}
+    end
   end
 
   def handle_info({:run_zip_search, zip_code}, socket) do
