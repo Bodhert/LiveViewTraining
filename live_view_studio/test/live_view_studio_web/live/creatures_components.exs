0ed2ca23fae95fe24e890320d74253ebfbc3fee0
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2022-11-20 15:07:26 +0000

    increasing coverage

---
+++ live_view_studio/test/live_view_studio_web/live/creatures_components.exs
@@ -0,0 +1,18 @@
+defmodule LiveViewStudioWeb.CreaturesComponentTest do
+  use LiveViewStudioWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+  alias LiveViewStudioWeb.CreaturesComponent
+
+  test "renders quote with 24-hour expiry by default" do
+    assigns = [
+      title: "test",
+      return_to: "/return_to"
+    ]
+
+    html = render_component(&CreaturesComponent.render/1, assigns)
+
+    assert html =~ "test"
+    assert html =~ "/return_to"
+  end
+end
