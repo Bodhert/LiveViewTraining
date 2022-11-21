49f08d784873f683f8004b6d7496b67fd17df63a
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2022-11-21 02:41:30 +0000

    more coverage increase

---
+++ live_view_studio/test/live_view_studio_web/live/map_live_test.exs
@@ -0,0 +1,26 @@
+defmodule LiveViewStudio.MapLiveTest do
+  use LiveViewStudioWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+  alias LiveViewStudio.Incidents
+
+  test "reporting an incident creates a new item", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/map")
+
+    view
+    |> element("button", "Report Incident")
+    |> render_click()
+
+    assert render(view) |> number_of_incidents == 1
+
+    view
+    |> element("button", "Report Incident")
+    |> render_click()
+
+    assert render(view) |> number_of_incidents == 2
+  end
+
+  defp number_of_incidents(html) do
+    html |> :binary.matches("phx-value-id") |> length()
+  end
+end
